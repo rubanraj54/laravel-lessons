2678ace90fd1720e6ef82003e62cfb9bf1643644
Rubanraj <rubanraj54@gmail.com>
2018-01-06 00:14:31 +0100

    Configuring mysql with laravel

---
+++ blog/resources/views/cache/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <title>Cache tutorial</title>
+    </head>
+    <body>
+        <div class="container">
+            <h2>Cache tutorial</h2>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <span>List of chocolates</span>
+                </div>
+                <div class="panel-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">Dark chocolate</li>
+                        <li class="list-group-item">Fantasy</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <script src="/js/manifest.js"></script>
+        <script src="/js/vendor.js"></script>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
---
+++ blog/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('cache.index');
+    return view('welcome');
+});
+
+
+Route::get('/test/', function () {
+    // return view('cache.index');
+    return view('test.index`');
+});
